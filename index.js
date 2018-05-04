17c102acdc4e4b7f4a32c3644e411b1783b43662
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-04 03:38:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(arrayOFMusicians, arrayOfInstruments) {
+  
+  var emptyArray=[];
+  
+  for(var i = 0; i < arrayOFMusicians.length; i++){
+    emptyArray.push(`${arrayOFMusicians[i]} plays ${arrayOfInstruments[i]}`);
+  }
+  return emptyArray;
+}
+
+function johnLennonFacts(arrayOfFacts){
+  var i = 0;
+  while(i < arrayOfFacts.length){
+    arrayOfFacts[i] = arrayOfFacts[i] + "!!!";
+    console.log(arrayOfFacts[i]);
+  }
+  return arrayOfFacts;
+}
